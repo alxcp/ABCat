98394a9a45ddf0ed4786f28db2b17d48ba9f125a
Alexey Chepelev <alxcp@users.noreply.github.com>
2018-12-11 13:46:07 +0200

    TorrentFile downloader to plugin
    Implemented: TorrentFile downloader logic was moved to plugins
    Added: qBittorrent downloader option

---
+++ ABCat.Plugins.Parsers.Rutracker/ExternalAppTorrentFileDownloaderBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Component.Infrastructure;
+using JetBrains.Annotations;
+
+namespace ABCat.Plugins.Parsers.Rutracker
+{
+    public abstract class ExternalAppTorrentFileDownloaderBase : ITorrentFileDownloaderPlugin
+    {
+        protected abstract string GetCommandLineArguments(string target, string destinationFolder);
+        [CanBeNull]
+        protected abstract string GetExternalAppPath();
+
+        public abstract string DisplayName { get; }
+
+        public virtual void Download(string target, string destinationFolder)
+        {
+            var commandLineArguments = GetCommandLineArguments(target, destinationFolder);
+            var appPath = GetExternalAppPath();
+            if (!appPath.IsNullOrEmpty())
+                Process.Start(appPath, commandLineArguments);
+        }
+
+        public bool IsExists => File.Exists(GetExternalAppPath());
+
+        public void Dispose()
+        {
+        }
+
+        public bool CheckForConfig(bool correct, out Config incorrectConfig)
+        {
+            incorrectConfig = null;
+            return true;
+        }
+    }
+}
